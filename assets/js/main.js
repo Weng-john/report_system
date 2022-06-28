dead26de4ba754314097375e70d8d7e12e300e01
Weng-john <59682713+Weng-john@users.noreply.github.com>
2022-06-28 16:14:50 +0800

    modify murderer select

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,54 @@
 var url= ["https://script.google.com/macros/s/AKfycbzzxeNclfL39dpKD3rPKFNHZz5zCLnwXLuTBr1jbaYmwDBcrbSa/exec", "https://script.google.com/macros/s/AKfycbwNjytDQVAaGOYKescVPRpXH5K3R1WYKJuxJoqX-8iLzrDBYOv_/exec"];
 
+function readCookie(){
+    var cookieObj = {};
+    var cookieAry = document.cookie.split(';');
+    var cookie;
+    for (var i=0, l=cookieAry.length; i<l; ++i) {
+        cookie = cookieAry[i].trim();
+        cookie = cookie.split('=');
+        cookieObj[cookie[0]] = cookie[1];
+    }
+    return cookieObj;
+}
+
+function setCookie(freq){
+    var time= new Date();
+    var hh= time.getHours();
+    var mm= time.getMinutes();
+    var ss= time.getSeconds();
+    var now= (hh*60+mm)*60+ss;
+    var curCookie= "submitTimes=" + freq + ";  max-age="+ (86400-now) + ";";
+    document.cookie= curCookie;
+}
+
 function Confirm(){
-    var murderer= document.getElementById("murderer").value;
-    var color= document.getElementById("color").value;
-    document.getElementById("murderer").value= "";
-    document.getElementById("color").value= "";
+    var murderer= $("input[name='suspect']:checked").val();
+    var color= $("#color").val();
+    $("#color").val("")
+    $("input[name='suspect']:checked").prop('checked', false);
+    
+    if(typeof(murderer)=="undefined"||color==""){   //確認是否兩格都有填
+        alert("兩個條件皆填寫才能回報喔！");
+        return;
+    }    
                                                     //確認是否送出資料
     var timeCheck= confirm("因事態緊急，此系統只限回報一次，確定要送出資料嗎？");
     if(!timeCheck)
         return;
     
-    var submitTime= document.getElementById("submitTime").value;
+    var cookieCon= readCookie();
+    var submitTime= cookieCon["submitTimes"];
     if(submitTime){                                 //確認是否送過資料
         alert("很抱歉，已超過回報次數");
         return;
     }
     
-    if(murderer==""||color==""){                    //確認是否兩格都有填
-        alert("兩個條件皆填寫才能回報喔");
-        return;
-    }
-    
+    setCookie(1);
     Send(murderer, color, 0);
 }
 
 function Send(murderer, color, index){
-    document.getElementById("submitTime").value= 1;
     $.ajax({
         type:'get',
         cache: false,
@@ -43,8 +66,10 @@ function Send(murderer, color, index){
                 alert("這段時間，總部已經找出真正的兇手及拆除炸彈的顏色順序。\n結果很抱歉，顏色順序的推論錯誤，但兇手正確。\n謝謝你的協助。");
             else if(respond=="murdererErrororderError")     //兇手、顏色順序皆錯誤
                 alert("這段時間，總部已經找出真正的兇手及拆除炸彈的顏色順序。\n結果很抱歉，兇手與顏色順序的推論皆錯誤\n謝謝你的協助。");
-            else                                            //解出正確的答案
+            else{                                           //解出正確的答案
                 alert("這段時間中，總部已經找出真正的兇手及拆除炸彈的顏色順序。\n恭喜，你的推論正確，很感謝你的協助。");
+                document.cookie= "submitTimes=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+            }
         },
         error: function(XMLHttpRequest, textStatus, errorThrown){
             Send(murderer, color, 1);
